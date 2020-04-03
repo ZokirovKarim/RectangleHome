1e96bb6950c8b28f9a026e29bbe719f91be2746a
Karim <karim.zokirov@mail.ru>
2020-04-03 17:30:58 +0500

    DZ Rectangle

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-
-namespace Klass.Home
+public class Rectangle
 {
-    class Program
+    double side1, side2;
+    double area,perimeter;
+     static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-           
-        }
+        var rectangle = new Rectangle(5.5,6.5);
+        Console.WriteLine($"Площадь: {rectangle.Area}, Прямоугольник: { rectangle.Perimeter}");
+        Console.ReadLine();
     }
-}gi
+    
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+    
+    public double AreaCalculator()
+    {
+        return this.area=side1*side2;
+    }
+    
+    public double PerimeterCalculator()
+    {
+        return  this.perimeter=(side1+side2)*2;
+    }
+    
+    public double Area
+    {
+        get { return AreaCalculator(); }
+    }
+    
+    public double Perimeter
+    {
+        get { return PerimeterCalculator(); }
+    }
+}
+
+   
+
+
+ 
